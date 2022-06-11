ebbd728c70fb2115cf0d665ecda13a46b2d8cb50
My Name <myname@example.com>
2022-06-11 12:14:07 +0900

    update2

--- hello2.py
+++ hello2.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("abcde")
 st.text("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+if st.button('abcde'):
+     st.write('fghij')
+ else:
+     st.write('あいうえお')
